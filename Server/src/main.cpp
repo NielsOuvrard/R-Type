8cd6eec6f08cba729fc5712c1a96c27c1b5f59d1
rclovis <clovis.rabot@epitech.eu>
2023-09-20 15:51:10 +0200

    🚧 Add frame per second limit

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -7,7 +7,12 @@
 
 #include <iostream>
 #include <haze.hpp>
+#include <chrono>
 #include "Engine.hpp"
+#include <thread>
+
+const double desiredFPS = 10.0;
+const double frameDuration = 1.0 / desiredFPS;
 
 int main()
 {
@@ -20,8 +25,20 @@ int main()
     entity->AddComponent(new Haze::Window(800, 600));
     entity->AddComponent(new Haze::Sprite("assets/ship.png"));
     engine.addEntity(entity);
+
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> currentTime, lastTime;
+    double lag = 0.0;
     while (1) {
+        auto startTime = std::chrono::high_resolution_clock::now();
         engine.update();
+        auto endTime = std::chrono::high_resolution_clock::now();
+        auto deltaTime = std::chrono::duration<double>(endTime - startTime).count();
+
+        if (deltaTime < frameDuration) {
+            double sleepTime = frameDuration - deltaTime;
+            std::this_thread::sleep_for(std::chrono::duration<double>(sleepTime));
+        }
     }
     entity->showComponents();
     return 0;
