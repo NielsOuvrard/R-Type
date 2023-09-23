4d68bd86107dba9d109cf6675ab460187820b2db
Corentin Fortes <corentin.fortes@epitech.eu>
2023-09-23 12:59:13 +0200

    🚧️ json et jeu que corentin avait commancé

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -10,38 +10,47 @@
 #include <chrono>
 #include "Engine.hpp"
 #include <thread>
+#include "Rtype.hpp"
 
-const double desiredFPS = 10.0;
-const double frameDuration = 1.0 / desiredFPS;
+// const double desiredFPS = 10.0;
+// const double frameDuration = 1.0 / desiredFPS;
 
-int main()
+// int main()
+// {
+//     std::cout << "Calling the engine!" << std::endl;
+//     Haze::Engine engine;
+//     engine.init();
+//     Haze::Entity *entity1 = engine.createEntity();
+//     Haze::Entity *entity2 = engine.createEntity();
+//     entity1->addComponent(new Haze::Position(0, 0));
+//     entity1->addComponent(new Haze::Sprite("assets/ship.png"));
+//     // entity1->AddComponent(new Haze::Velocity(1, 0));
+//     entity1->addComponent(new Haze::Sprite("assets/ship.png"));
+//     std::vector<std::vector<std::string>> params = {{"z", "0","1"}, {"s", "0","-1"}, {"d", "1","0"}, {"q", "-1","0"}};
+//     entity1->addComponent(new Haze::VelocityOnClick(params));
+//     // entity1->AddComponent(new Haze::Window(800, 600));
+
+//     entity2->addComponent(new Haze::Window(800, 600));
+//     std::chrono::time_point<std::chrono::high_resolution_clock> currentTime, lastTime;
+//     double lag = 0.0;
+//     while (1) {
+//         auto startTime = std::chrono::high_resolution_clock::now();
+//         engine.update();
+//         auto endTime = std::chrono::high_resolution_clock::now();
+//         auto deltaTime = std::chrono::duration<double>(endTime - startTime).count();
+//         if (deltaTime < frameDuration) {
+//             double sleepTime = frameDuration - deltaTime;
+//             std::this_thread::sleep_for(std::chrono::duration<double>(sleepTime));
+//         }
+//     }
+//     entity1->showComponents();
+//     return 0;
+// }
+
+int main ()
 {
+    Rtype rtype;
     std::cout << "Calling the engine!" << std::endl;
-    Haze::Engine engine;
-    engine.init();
-    Haze::Entity *entity1 = engine.createEntity();
-    Haze::Entity *entity2 = engine.createEntity();
-    entity1->addComponent(new Haze::Position(0, 0));
-    entity1->addComponent(new Haze::Sprite("assets/ship.png"));
-    // entity1->AddComponent(new Haze::Velocity(1, 0));
-    entity1->addComponent(new Haze::Sprite("assets/ship.png"));
-    std::vector<std::vector<std::string>> params = {{"z", "0","1"}, {"s", "0","-1"}, {"d", "1","0"}, {"q", "-1","0"}};
-    entity1->addComponent(new Haze::VelocityOnClick(params));
-    // entity1->AddComponent(new Haze::Window(800, 600));
-
-    entity2->addComponent(new Haze::Window(800, 600));
-    std::chrono::time_point<std::chrono::high_resolution_clock> currentTime, lastTime;
-    double lag = 0.0;
-    while (1) {
-        auto startTime = std::chrono::high_resolution_clock::now();
-        engine.update();
-        auto endTime = std::chrono::high_resolution_clock::now();
-        auto deltaTime = std::chrono::duration<double>(endTime - startTime).count();
-        if (deltaTime < frameDuration) {
-            double sleepTime = frameDuration - deltaTime;
-            std::this_thread::sleep_for(std::chrono::duration<double>(sleepTime));
-        }
-    }
-    entity1->showComponents();
+    rtype.run();
     return 0;
 }
