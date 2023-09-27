185b1ab3613c7e08aca4270c6c06c263c189e7af
rclovis <clovis.rabot@epitech.eu>
2023-09-27 09:44:03 +0200

    🚑 Bug fix

--- Client/src/Rttype.cpp
+++ Client/src/Rttype.cpp
@@ -6,12 +6,9 @@
 */
 
 #include "Rttype.hpp"
-#include <haze.hpp>
 
 Rttype::Rttype()
 {
-    std::cout << "hello" << std::endl;
-    // engine = new Haze::Engine;
     engine.init();
 
     entityVortex->addComponent(new Haze::Position(120, 200));
@@ -66,80 +63,80 @@ void Rttype::moveRight(void *component)
 
 void Rttype::run()
 {
-    while (engine.isOpen())
-    {
-        sf::Event event;
-        if (!isMoving)
-        {
-            velocityPlayer->x = 0;
-            velocityPlayer->y = 0;
-        }
-        else if (isMoving == 'U')
-        {
-            velocityPlayer->y += -1;
-        }
-        else if (isMoving == 'D')
-        {
-            velocityPlayer->y += 1;
-        }
-        else if (isMoving == 'L')
-        {
-            velocityPlayer->x += -1;
-        }
-        else if (isMoving == 'R')
-        {
-            velocityPlayer->x += 1;
-        }
-        while (window->window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window->window.close();
-            }
-            if (event.key.code == sf::Keyboard::Enter)
-            {
-                Haze::Entity *newVortex = engine.createEntity();
-                auto position = static_cast<Haze::Position *>(entitySpaceship->getComponent("Position"));
-                newVortex->addComponent(new Haze::Position(position->x, position->y));
-                newVortex->addComponent(new Haze::Velocity(2, 0));
-                newVortex->addComponent(new Haze::Size(34 * 3, 34 * 3));
-                newVortex->addComponent(vortexSprite);
-                newVortex->addComponent(new Haze::Animation(*vortexSprite, 0, 0, 34, 34, 3, 1));
-            }
+    // while (engine.isOpen())
+    // {
+    //     sf::Event event;
+    //     if (!isMoving)
+    //     {
+    //         velocityPlayer->x = 0;
+    //         velocityPlayer->y = 0;
+    //     }
+    //     else if (isMoving == 'U')
+    //     {
+    //         velocityPlayer->y += -1;
+    //     }
+    //     else if (isMoving == 'D')
+    //     {
+    //         velocityPlayer->y += 1;
+    //     }
+    //     else if (isMoving == 'L')
+    //     {
+    //         velocityPlayer->x += -1;
+    //     }
+    //     else if (isMoving == 'R')
+    //     {
+    //         velocityPlayer->x += 1;
+    //     }
+    //     while (window->window.pollEvent(event))
+    //     {
+    //         if (event.type == sf::Event::Closed)
+    //         {
+    //             window->window.close();
+    //         }
+    //         if (event.key.code == sf::Keyboard::Enter)
+    //         {
+    //             Haze::Entity *newVortex = engine.createEntity();
+    //             auto position = static_cast<Haze::Position *>(entitySpaceship->getComponent("Position"));
+    //             newVortex->addComponent(new Haze::Position(position->x, position->y));
+    //             newVortex->addComponent(new Haze::Velocity(2, 0));
+    //             newVortex->addComponent(new Haze::Size(34 * 3, 34 * 3));
+    //             newVortex->addComponent(vortexSprite);
+    //             newVortex->addComponent(new Haze::Animation(*vortexSprite, 0, 0, 34, 34, 3, 1));
+    //         }
 
-            // keypressed
-            if (event.type == sf::Event::KeyPressed)
-            {
-                if (event.key.code == sf::Keyboard::Up)
-                {
-                    isMoving = 'U';
-                }
-                if (event.key.code == sf::Keyboard::Left)
-                {
-                    isMoving = 'L';
-                }
-                if (event.key.code == sf::Keyboard::Down)
-                {
-                    isMoving = 'D';
-                }
-                if (event.key.code == sf::Keyboard::Right)
-                {
-                    isMoving = 'R';
-                }
-            }
-            // keyreleased
-            if (event.type == sf::Event::KeyReleased)
-            {
-                if (event.key.code == sf::Keyboard::Up)
-                    isMoving = '\0';
-                if (event.key.code == sf::Keyboard::Left)
-                    isMoving = '\0';
-                if (event.key.code == sf::Keyboard::Down)
-                    isMoving = '\0';
-                if (event.key.code == sf::Keyboard::Right)
-                    isMoving = '\0';
-            }
-        }
-        engine.update();
-    }
+    //         // keypressed
+    //         if (event.type == sf::Event::KeyPressed)
+    //         {
+    //             if (event.key.code == sf::Keyboard::Up)
+    //             {
+    //                 isMoving = 'U';
+    //             }
+    //             if (event.key.code == sf::Keyboard::Left)
+    //             {
+    //                 isMoving = 'L';
+    //             }
+    //             if (event.key.code == sf::Keyboard::Down)
+    //             {
+    //                 isMoving = 'D';
+    //             }
+    //             if (event.key.code == sf::Keyboard::Right)
+    //             {
+    //                 isMoving = 'R';
+    //             }
+    //         }
+    //         // keyreleased
+    //         if (event.type == sf::Event::KeyReleased)
+    //         {
+    //             if (event.key.code == sf::Keyboard::Up)
+    //                 isMoving = '\0';
+    //             if (event.key.code == sf::Keyboard::Left)
+    //                 isMoving = '\0';
+    //             if (event.key.code == sf::Keyboard::Down)
+    //                 isMoving = '\0';
+    //             if (event.key.code == sf::Keyboard::Right)
+    //                 isMoving = '\0';
+    //         }
+    //     }
+    //     engine.update();
+    // }
 }
