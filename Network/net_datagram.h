3e3381a66ed4ff0919d1250cc8d3138ffd72bde0
Erwan Gonzales <erwan.gonzales@epitech.eu>
2023-10-10 17:24:46 +0200

    easier to construct messages and datagram

--- Network/net_datagram.h
+++ Network/net_datagram.h
@@ -6,9 +6,11 @@
 
 #include "net_common.h"
 #include <algorithm>
+#include <chrono>
 #include <cstdint>
 #include <iomanip>
 #include <iostream>
+
 namespace network {
 
     template<typename T>
@@ -22,6 +24,9 @@ namespace network {
     struct datagram {
         datagram_header<T> header;
         std::array<uint8_t, 1024 - sizeof(datagram_header<T>)> body;
+
+        datagram() = default;
+        explicit datagram(T id) : header({id, 0, std::chrono::system_clock::now()}) {}
     };
 
     template<typename T>
