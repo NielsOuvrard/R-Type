90f77b4789795e18fe344fd98c28f03995e0da2e
rclovis <clovis.rabot@epitech.eu>
2023-10-20 20:26:21 +0200

    Asset manager in engine

---
+++ Haze/AssetManager/AssetManager.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <map>
+#include <memory>
+#include "IDisplay.hpp"
+
+namespace Haze
+{
+    template <typename T>
+    class AssetManager {
+        public:
+            AssetManager() = default;
+            ~AssetManager() = default;
+            std::shared_ptr<T> loadTexture(std::string path)
+            {
+                if (_textures.find(path) == _textures.end()) {
+                    _textures[path] = std::make_shared<T>(path);
+                }
+                return _textures[path];
+            }
+        protected:
+        private:
+            std::map<std::string, std::shared_ptr<T>> _textures;
+
+    };
+}
