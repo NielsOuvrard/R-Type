ecc8ea6c951c5b1233a27d4c05d126d8277f1407
Erwan Gonzales <erwan.gonzales@epitech.eu>
2023-10-23 18:01:41 +0200

    added name field to main menu

--- Client/Scenes/Login.cpp
+++ Client/Scenes/Login.cpp
@@ -10,18 +10,24 @@ Login::Login(Haze::Engine &engine) : Element(engine) {}
 
 void Login::build()
 {
-    _children["logo"] = std::make_shared<Image>(_engine, "assets/sprites/haze_logo.png", AxisPair{150.0, 70.0}, AxisPair{0.7, 0.7});
+    _children["logo"] = std::make_shared<Image>(_engine, "assets/sprites/haze_logo.png", AxisPair{150.0, 40.0}, AxisPair{0.7, 0.7});
     _children["logo"]->build();
 
-    _children["ip_img"] = std::make_shared<Image>(_engine, "assets/sprites/button.png", AxisPair{210, 276}, AxisPair{1, 1});
+    _children["name_img"] = std::make_shared<Image>(_engine, "assets/sprites/button.png", AxisPair{210, 220}, AxisPair{1, 1});
+    _children["name_img"]->build();
+    _children["name"] = std::make_shared<TextInput>(_engine, "name", AxisPair{250, 220 + 25});
+    _children["name"]->build();
+    get<TextInput>("name")->setHitbox(-20, -20, 320, 81);
+
+    _children["ip_img"] = std::make_shared<Image>(_engine, "assets/sprites/button.png", AxisPair{210, 345}, AxisPair{1, 1});
     _children["ip_img"]->build();
-    _children["ip"] = std::make_shared<TextInput>(_engine, "ip", AxisPair{250, 300});
+    _children["ip"] = std::make_shared<TextInput>(_engine, "ip", AxisPair{250, 345 + 25});
     _children["ip"]->build();
     get<TextInput>("ip")->setHitbox(-20, -20, 320, 81);
 
-    _children["port_img"] = std::make_shared<Image>(_engine, "assets/sprites/button.png", AxisPair{210, 426}, AxisPair{1, 1});
+    _children["port_img"] = std::make_shared<Image>(_engine, "assets/sprites/button.png", AxisPair{210, 470}, AxisPair{1, 1});
     _children["port_img"]->build();
-    _children["port"] = std::make_shared<TextInput>(_engine, "port", AxisPair{250, 450});
+    _children["port"] = std::make_shared<TextInput>(_engine, "port", AxisPair{250, 470 + 25});
     _children["port"]->build();
     get<TextInput>("port")->setHitbox(-20, -20, 320, 81);
 
