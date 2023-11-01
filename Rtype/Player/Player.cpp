820cba6f00d3967c7660d07eb8bb5f2f83aba7e1
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2023-11-01 14:49:08 +0100

    🚑 forgot change

--- Rtype/Player/Player.cpp
+++ Rtype/Player/Player.cpp
@@ -112,7 +112,7 @@ void Player::send()
     _dataGame.channel.sendGroup(RType::message::addComponent(_entity->getId(), "Position", new Haze::PositionData{pos->x, pos->y}, sizeof(Haze::PositionData)));
     _dataGame.channel.sendGroup(RType::message::addComponent(_entity->getId(), "Scale", new Haze::ScaleData{UNIVERSAL_SCALE, UNIVERSAL_SCALE}, sizeof(Haze::ScaleData)));
     _dataGame.channel.sendGroup(RType::message::addComponent(_entity->getId(), "Hitbox", new Haze::HitboxData{hitbox}, sizeof(Haze::HitboxData)));
-    _dataGame.channel.sendGroup(RType::message::addComponent(_entity->getId(), "Interpolation", new Haze::InterpolationData{_engine.getFramerate()}, sizeof(Haze::InterpolationData)));
+    _dataGame.channel.sendGroup(RType::message::addComponent(_entity->getId(), "Interpolation", new Haze::InterpolationData{_dataGame.engine.getFramerate()}, sizeof(Haze::InterpolationData)));
     //_dataGame.channel.sendGroup(RType::message::addComponent(_entity->getId(), "HitboxDisplay", nullptr, 0));
     _dataGame.channel.sendGroup(RType::message::addComponent(_entity->getId(), "Sprite", new Haze::SpriteData{"assets/sprites/spaceships.png"}, sizeof(Haze::SpriteData)));
     if (_id == 1) {
